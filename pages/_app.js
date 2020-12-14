c4aaec7acc3a76b59a73f441ca0d9477ba3a6df2
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2020-12-14 12:54:57 +0800

    添加scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../apollo/client'
+import '../styles/index.scss'
+import 'antd/dist/antd.css'
 
-export default function App({ Component, pageProps }) {
+export default function App ({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
