1225d1c527d0ea831fdbeb98faf031379fa53006
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2020-12-24 21:45:56 +0800

    配置文件

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
+  env: {
+    SQLITE3_DB_FILEPATH: process.env.SQLITE3_DB_FILEPATH
+  },
   reactStrictMode: true
 }
